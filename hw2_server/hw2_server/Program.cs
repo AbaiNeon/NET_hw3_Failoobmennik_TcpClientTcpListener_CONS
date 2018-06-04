6971de0248d9cff1b1412320ec0a66f174170699
ТогузбаевА <abai2010@bk.ru>
2018-06-04 17:17:26 +0600

    initCommit

---
+++ hw2_server/hw2_server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace hw2_server
+{
+    class Program
+    {
+        static string path = @"C:\images\";
+        static void Main(string[] args)
+        {
+            
+
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 3535);
+            try
+            {
+                server.Start();
+                //int name = 1;
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+
+                    Task task = new Task(() => Proccess(client));
+                    task.Start();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex2)
+            {
+                Console.WriteLine(ex2.Message);
+            }
+            finally
+            {
+                server.Stop();
+                Console.ReadLine();
+            }
+        }
+
+        private static void Proccess(TcpClient client)
+        {
+            int bytes;
+            byte[] buffer = new byte[1024];
+            StringBuilder builder = new StringBuilder();
+            MemoryStream ms = new MemoryStream();
+
+            using (var networkStream = client.GetStream())
+            {
+                do
+                {
+                    bytes = networkStream.Read(buffer, 0, buffer.Length);
+                    ms.Write(buffer, 0, buffer.Length);
+                }
+                while (networkStream.DataAvailable);
+            }
+
+            using (Image image = Image.FromStream(ms))
+            {
+                image.Save(path + "output.jpg");  // Or Png
+            }
+
+            ms.Close();
+            client.Close();
+        }
+    }
+}
---
+++ hw3_client/hw3_client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace hw3_client
+{
+    class Program
+    {
+        private const int port = 3535;
+        private const string server = "127.0.0.1";
+        
+        static void Main(string[] args)
+        {
+            string path1 = @"http://bipbap.ru/wp-content/uploads/2017/09/Cool-High-Resolution-Wallpaper-1920x1080.jpg";
+            string path2 = @"http://elitefon.ru/images/201211/elitefon.ru_6492.jpg";
+            string path3 = @"https://ribalych.ru/wp-content/uploads/2017/05/prikoly_000-17.jpg";
+
+            try
+            {
+                //Image to byte array from a url
+                byte[] imageBytes;
+                using (WebClient webClient = new WebClient())
+                {
+                    imageBytes = webClient.DownloadData(path3);
+                }
+
+                TcpClient client = new TcpClient();
+                client.Connect(server, port);
+
+                NetworkStream stream = client.GetStream();
+                //var buffer = Encoding.Default.GetBytes(path1);
+
+                stream.Write(imageBytes, 0, imageBytes.Length);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //Console.Read();
+        }
+    }
+}
